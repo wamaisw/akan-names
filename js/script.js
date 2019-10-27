2782c7e01dbfdd0758ff5c33b4c2ea4d601b277d
steve.wamai <wamaisw@gmail.com>
2019-10-27 11:10:40 +0300

    added initial structure to script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var akanMales = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var akanFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (5 + 1) / 10)) + 9) % 7;
+
+function validate() {
+    ("datepicker").datepicker({maxDate: today});
+  }
