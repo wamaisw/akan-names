b4d8c56ff8793fd0e091c64481356300e27d7b0f
steve.wamai <wamaisw@gmail.com>
2019-10-28 16:53:32 +0300

    pushing all changes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
-var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var day;
 function dateOfWeek() {
-  var dateOfBirth = document.getElementById("dateOfBirth").value;
-  var CC = parseInt(dateOfBirth.substring(0,2));
-  var YY = parseInt(dateOfBirth.substring(2,4));
-  var MM = parseInt(dateOfBirth.substring(5,7));
-  var DD = parseInt(dateOfBirth.substring(8,10));
+  
+  var Y = parseInt(document.getElementById("year").value);
+  var M = parseInt(document.getElementById("month").value)
+  var D = parseInt(document.getElementById("day").value);
+  var gender = document.getElementsByName("gender");
 
-d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (5 + 1) / 10)) + 9) % 7;
-
-function validate() {
-    ("datepicker").datepicker({maxDate: today});
+  for(y=0;y<gender.length;y++){
+    if(gender[y].checked){
+      var gender1 = gender[y].value;
+    }
   }
-day = Math.floor(d)-1;
-
-  daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var option = document.getElementsByName('name').value;
-  var femaleGender = document.getElementById("femaleGender").value;
-
-
-  if (option == "output1") {
-    document.getElementById("akanOutput").innerHTML = "You were born on " + dayOfTheWeek[day] + " your Akan name is " + akanMales[day];
-    console.log(dayOfTheWeek[day]);
-    console.log(akanMales[day]);
+  alert(gender1);
+ 
+  if(M<1||M>12||Y<1500||Y>2700||D<1||D>31){
+    alert("Not a valid date")
   }
-  else {
-    document.getElementById("akanOutput").innerHTML = "You were born on " + dayOfTheWeek[day] + " your Akan name is " + akanFemales[day];
-    console.log(dayOfTheWeek[day]);
-    console.log(akanFemales[day]);
+  
+  var C = Y.toString();
+  var CC = C[0].concat(C[1]);
+  var YY = C[2].concat(C[3]);
+  var CC = parseInt(CC);
+  var YY = parseInt(YY);
+
+  var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+  var wd = Math.trunc(((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * M + 1) / 10)) + D) % 7);
+  
+
+  for(x=0;x<7;x++){
+    if(x===wd && gender1==="male"){
+      var akan = akanMaleNames[x];
+    }
+    else if(x===wd && gender1==="female"){
+      var akan = akanFemaleNames[x];
+    }
   }
+  alert(akan);
+  alert(wd);
+
+}
+
